8d40dcfe74219f0e39dc24dd7beba7f6b0286ae1
Arif Rabbani <arif.93rabbani@gmail.com>
2023-03-12 00:07:37 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Clock } from './Clock';
 
 function App() {
   return (
-    <div className="flex h-full grow items-center justify-center bg-alpha-500">
-      <div className="h-full w-full shadow-lg md:h-5/6 md:w-3/4 lg:w-1/4">
+    <div className="flex h-full items-center justify-center bg-alpha-500">
+      <div className="h-full w-full shadow-lg md:h-5/6 md:w-3/4 lg:w-2/4 xl:w-1/4 2xl:w-1/6">
         <Clock />
       </div>
     </div>
