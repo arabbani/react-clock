39d85dfd092992a2f24be67e641b28385466904a
Arif Rabbani <arif.93rabbani@gmail.com>
2023-03-12 01:02:49 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { BottomMenu } from './components/BottomMenu';
 import { Clock } from './pages/Clock';
 
 function App() {
@@ -6,7 +5,6 @@ function App() {
     <div className="flex h-full items-center justify-center bg-alpha-500">
       <div className="flex h-full w-full flex-col justify-between py-4 px-6 shadow-lg md:h-5/6 md:w-2/5 lg:w-4/12 xl:w-1/4 2xl:w-1/6">
         <Clock />
-        <BottomMenu />
       </div>
     </div>
   );
--- src/components/BottomMenu.tsx
+++
@@ -1,9 +0,0 @@
-import { FaStopwatch } from 'react-icons/fa';
-
-export function BottomMenu() {
-  return (
-    <div className="flex justify-end">
-      <FaStopwatch className="fill-beta-500 text-2xl" />
-    </div>
-  );
-}
