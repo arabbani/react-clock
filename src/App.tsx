7967320c8febeca50f55009322993928d6a2de35
Arif Rabbani <arif.93rabbani@gmail.com>
2023-03-12 00:18:38 +0530

    css tuning

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Clock } from './Clock';
 function App() {
   return (
     <div className="flex h-full items-center justify-center bg-alpha-500">
-      <div className="h-full w-full shadow-lg md:h-5/6 md:w-3/4 lg:w-2/4 xl:w-1/4 2xl:w-1/6">
+      <div className="h-full w-full shadow-lg md:h-5/6 md:w-2/5 lg:w-4/12 xl:w-1/4 2xl:w-1/6">
         <Clock />
       </div>
     </div>
