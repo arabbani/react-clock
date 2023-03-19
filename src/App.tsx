795e36d4dd72a5003684814aa9ef739c9bc02f2a
Arif Rabbani <arif.93rabbani@gmail.com>
2023-03-19 10:02:44 +0530

    improvement

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="mx-auto max-w-5xl sm:container">
       <div className="flex grow justify-center">
-        <div className="h-screen w-full sm:w-3/6 md:w-2/5 lg:w-4/12">
+        <div className="h-screen w-full sm:w-3/6 md:w-2/5 lg:w-3/12">
           <Clock />
         </div>
       </div>
